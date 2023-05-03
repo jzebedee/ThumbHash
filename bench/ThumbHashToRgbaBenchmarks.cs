72e94a7ac8a4b2e7b1fb8a65fb03648d79eae751
J. Zebedee <zebedee@code.gripe>
2023-05-02 19:48:38 -0500

    Split out ThumbHashToRgbaBenchmarks

---
+++ bench/ThumbHashToRgbaBenchmarks.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnosers;
+using System;
+using System.Collections.Generic;
+
+namespace ThumbHash.Benchmarks;
+
+[MemoryDiagnoser]
+[HardwareCounters(HardwareCounter.BranchInstructions, HardwareCounter.BranchMispredictions)]
+public class ThumbHashToRgbaBenchmarks
+{
+    private static byte[] FlowerThumbHash => Convert.FromHexString("934A062D069256C374055867DA8AB6679490510719");
+    private static byte[] TuxThumbHash => Convert.FromHexString("A1198A1C02383A25D727F68B971FF7F9717F80376758987906");
+
+    public static IEnumerable<object> ThumbHashes_NoAlpha
+    {
+        get
+        {
+            yield return FlowerThumbHash;
+        }
+    }
+
+    public static IEnumerable<object> ThumbHashes_Alpha
+    {
+        get
+        {
+            yield return TuxThumbHash;
+        }
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(ThumbHashes_NoAlpha))]
+    public (int,int,byte[]) ThumbHashToRgba_NoAlpha(byte[] thumbhash) => ThumbHash.ThumbHashToRgba(thumbhash);
+
+    [Benchmark]
+    [ArgumentsSource(nameof(ThumbHashes_Alpha))]
+    public (int, int, byte[]) ThumbHashToRgba_Alpha(byte[] thumbhash) => ThumbHash.ThumbHashToRgba(thumbhash);
+}
