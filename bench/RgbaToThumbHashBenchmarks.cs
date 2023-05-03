6d58c4b13e0171b8602bb1ba22a1615ef51fa13d
J. Zebedee <zebedee@code.gripe>
2023-05-02 19:48:38 -0500

    Split out ThumbHashToRgbaBenchmarks

--- bench/RgbaToThumbHashBenchmarks.cs
+++ bench/RgbaToThumbHashBenchmarks.cs
@@ -1,7 +1,6 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Diagnosers;
 using SkiaSharp;
-using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -38,7 +37,7 @@ public class RgbaToThumbHashBenchmarks
 
     private static readonly SKBitmap Flower = GetBitmap(Path.Join(AssetBase, "flower.jpg"));
 
-    private static readonly SKBitmap Tux = GetBitmap(Path.Join(AssetBase, "tux.png"));
+    private static readonly SKBitmap Tux = GetBitmap(Path.Join(AssetBase, "tux.png"), fixPremul: true);
 
     public static IEnumerable<object> Images_NoAlpha
     {
@@ -56,26 +55,6 @@ public class RgbaToThumbHashBenchmarks
         }
     }
 
-    private static byte[] FlowerThumbHash => Convert.FromHexString("934A062D069256C374055867DA8AB6679490510719");
-    private static byte[] TuxThumbHash => Convert.FromHexString("A1198A1C02383A25D727F68B971FF7F9717F80376758987906");
-
-    public static IEnumerable<object> ThumbHashes_NoAlpha
-    {
-        get
-        {
-            yield return FlowerThumbHash;
-        }
-    }
-
-    public static IEnumerable<object> ThumbHashes_Alpha
-    {
-        get
-        {
-            yield return TuxThumbHash;
-        }
-    }
-
-
     [Benchmark]
     [ArgumentsSource(nameof(Images_NoAlpha))]
     public int RgbaToThumbHash_NoAlpha(SKBitmap image) => ThumbHash.RgbaToThumbHash(stackalloc byte[25], image.Width, image.Height, image.GetPixelSpan());
@@ -83,12 +62,4 @@ public class RgbaToThumbHashBenchmarks
     [Benchmark]
     [ArgumentsSource(nameof(Images_Alpha))]
     public int RgbaToThumbHash_Alpha(SKBitmap image) => ThumbHash.RgbaToThumbHash(stackalloc byte[25], image.Width, image.Height, image.GetPixelSpan());
-
-    [Benchmark]
-    [ArgumentsSource(nameof(ThumbHashes_NoAlpha))]
-    public (int,int,byte[]) ThumbHashToRgba_NoAlpha(byte[] thumbhash) => ThumbHash.ThumbHashToRgba(thumbhash);
-
-    [Benchmark]
-    [ArgumentsSource(nameof(ThumbHashes_Alpha))]
-    public (int, int, byte[]) ThumbHashToRgba_Alpha(byte[] thumbhash) => ThumbHash.ThumbHashToRgba(thumbhash);
 }
