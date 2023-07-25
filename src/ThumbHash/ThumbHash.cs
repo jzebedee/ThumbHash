b43e4a29f83d63aab9d597f1f53b19285f659873
J. Zebedee <zebedee@code.gripe>
2023-07-24 22:10:52 -0500

    Add friendly ThumbHash type

---
+++ src/ThumbHash/ThumbHash.cs
@@ -0,0 +1,38 @@
+﻿namespace ThumbHashes;
+
+using static Utilities;
+
+public readonly record struct ThumbHash(ReadOnlyMemory<byte> Hash)
+{
+    /// <summary>
+    /// Extracts the approximate aspect ratio of the original image.
+    /// </summary>
+    public float ApproximateAspectRatio => ThumbHashToApproximateAspectRatio(Hash.Span);
+
+    /// <summary>
+    /// Extracts the average color from a ThumbHash.
+    /// </summary>
+    /// <returns>Unpremultiplied RGBA values where each value ranges from 0 to 1. </returns>
+    public (float r, float g, float b, float a) AverageColor => ThumbHashToAverageRgba(Hash.Span);
+
+    /// <summary>
+    /// Decodes a ThumbHash to an RGBA image.
+    /// </summary>
+    /// <returns>Width, height, and unpremultiplied RGBA8 pixels of the rendered ThumbHash.</returns>
+    public (int width, int height, byte[] rgba) ToImage() => ThumbHashToRgba(Hash.Span);
+
+    /// <summary>
+    /// Decodes a ThumbHash to an RGBA image.
+    /// </summary>
+    /// <returns>Width, height, and unpremultiplied RGBA8 pixels of the rendered ThumbHash.</returns>
+    public (int width, int height) ToImage(Span<byte> rgba) => ThumbHashToRgba(Hash.Span, rgba);
+
+    /// <summary>
+    /// Encodes an RGBA image to a ThumbHash.
+    /// </summary>
+    /// <param name="width">The width of the input image. Must be ≤100px.</param>
+    /// <param name="height">The height of the input image. Must be ≤100px.</param>
+    /// <param name="rgba">The pixels in the input image, row-by-row. RGB should not be premultiplied by A. Must have `w*h*4` elements.</param>
+    public static ThumbHash FromImage(int width, int height, ReadOnlySpan<byte> rgba)
+        => new(RgbaToThumbHash(width, height, rgba));
+}
---
+++ test/ThumbHash.Tests/ThumbHashTests.cs
@@ -0,0 +1,56 @@
+﻿using SkiaSharp;
+
+namespace ThumbHashes.Tests;
+using static Resources;
+
+public class ThumbHashTests
+{
+    public static IEnumerable<object[]> TestThumbHashes
+    {
+        get
+        {
+            yield return new object[] { FlowerThumbHash, FlowerThumbHashRendered, FlowerBitmap, FlowerAspectRatio, FlowerThumbHashAverages };
+            yield return new object[] { TuxThumbHash, TuxThumbHashRendered, TuxBitmap, TuxAspectRatio, TuxThumbHashAverages };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(TestThumbHashes))]
+    public void ValidateThumbHash(byte[] hash, SKBitmap rendered, SKBitmap original, float aspectRatio, (float r, float g, float b, float a) averages)
+    {
+        using (rendered)
+        using (original)
+        {
+            var th = new ThumbHash(hash);
+            Assert.Equal(hash, th.Hash);
+            Assert.Equal(aspectRatio, th.ApproximateAspectRatio);
+            Assert.Equal(averages, th.AverageColor);
+
+            var (w, h, thRenderedBytes) = th.ToImage();
+            Assert.Equal(rendered.Width, w);
+            Assert.Equal(rendered.Height, h);
+
+            var spanBytes = new byte[w * h * 4];
+            _ = th.ToImage(spanBytes);
+            Assert.Equal(thRenderedBytes, spanBytes);
+
+            {
+                using var hash_img = SKImage.FromPixelCopy(new(w, h, SKColorType.Rgba8888, SKAlphaType.Unpremul), thRenderedBytes);
+                using var hash_data_png = hash_img.Encode(SKEncodedImageFormat.Png, 100);
+                using var hash_bmp = SKBitmap.Decode(hash_data_png);
+
+                Assert.Equal(rendered.Pixels, hash_bmp.Pixels);
+            }
+
+            {
+                var thRecreated = ThumbHash.FromImage(original.Width, original.Height, original.GetPixelSpan());
+                //can't just compare th <-> thRecreated because
+                //ROM/ROS's equality does not check contents
+                // "This tests if two ReadOnlySpan<T> instances point to the same starting memory location,
+                // and have the same Length values. This does not compare the contents of two ReadOnlySpan<T> instances."
+                // https://learn.microsoft.com/en-us/dotnet/api/system.readonlyspan-1.op_equality?view=net-7.0#remarks
+                Assert.True(th.Hash.Span.SequenceEqual(thRecreated.Hash.Span));
+            }
+        }
+    }
+}
