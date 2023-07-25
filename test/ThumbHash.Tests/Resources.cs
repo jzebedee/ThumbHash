058a199cbdbcf241cf555cd685b17766fa6af724
J. Zebedee <zebedee@code.gripe>
2023-07-24 23:44:43 -0500

    Target net481 in tests

--- test/ThumbHash.Tests/Resources.cs
+++ test/ThumbHash.Tests/Resources.cs
@@ -4,9 +4,24 @@ namespace ThumbHashes.Tests;
 
 internal static class Resources
 {
+    private static byte[] FromHexString(string s)
+#if NET6_0_OR_GREATER
+        => Convert.FromHexString(s);
+#else
+    {
+        //this is only used for tests, so we can do it the slow and awful way
+        var ret = new byte[s.Length >> 1];
+        for (int i = 0; i < s.Length; i += 2)
+        {
+            ret[i >> 1] = Convert.ToByte(s.Substring(i, 2), 16);
+        }
+        return ret;
+    }
+#endif
+
     internal static SKBitmap FlowerBitmap => GetBitmap("Resources/flower.jpg");
 
-    internal static byte[] FlowerThumbHash => Convert.FromHexString("934A062D069256C374055867DA8AB6679490510719");
+    internal static byte[] FlowerThumbHash => FromHexString("934A062D069256C374055867DA8AB6679490510719");
 
     internal static (float r, float g, float b, float a) FlowerThumbHashAverages => (r: 0.484127015f, g: 0.341269821f, b: 0.0793650597f, a: 1f);
 
@@ -16,7 +31,7 @@ internal static class Resources
 
     internal static SKBitmap TuxBitmap => GetBitmap("Resources/tux.png", fixPremul: true);
 
-    internal static byte[] TuxThumbHash => Convert.FromHexString("A1198A1C02383A25D727F68B971FF7F9717F80376758987906");
+    internal static byte[] TuxThumbHash => FromHexString("A1198A1C02383A25D727F68B971FF7F9717F80376758987906");
 
     internal static (float r, float g, float b, float a) TuxThumbHashAverages => (r: 0.616402208f, g: 0.568783104f, b: 0.386243373f, a: 0.533333361f);
 
